7b4101c9459f843d220192a3979f7ded0e0c2357
Frank <fauletto@gmail.com>
2021-05-03 19:04:55 -0400

    Add files via upload

--- npc-generator/js/app.js
+++ npc-generator/js/app.js
@@ -3,39 +3,75 @@ const nameInput = document.querySelector('#char-name');
 const heading = document.querySelector('#main-heading')
 const classInput = document.querySelector('#char-class')
 const subheading = document.querySelector('#class-h2');
-const diceRollButton = document.querySelector('#rollAll');
-const diceDisplays = document.querySelector('#diceDisplayH2')
-// Update H1 with character name in real time
-nameInput.addEventListener('input', function (e) {
-  if(nameInput.value == ''){
-    heading.innerText = "RPG Character Generator";
-  }else{
-    heading.innerText = `Character Sheet for ${nameInput.value}`
-  }
-  console.log(nameInput.value)
-})
-// Update H2 with character class in real time
-classInput.addEventListener('input', function (e) {
-  if(classInput.value == ''){
-    subheading.innerText = "";
-  }else{
-    subheading.innerText = `the ${classInput.value}`
-  }
-  console.log(nameInput.value)
-});
+const randomizeAllButton = document.querySelector('#randomizeAll');
+const biography = document.querySelector('#backgroundInfo');
 
-// Get random number from 1-6
-let getRandNumber = () =>{
-  return Math.floor(Math.random() * 6 + 1);
+// Get random number from 0-n
+let getRandNumber = (n) =>{
+  return Math.floor(Math.random() * n);
 }
 
-// Roll dice
-diceRollButton.addEventListener('click', function(){
-  for(let i = 0; i <4; i++){
-    let roll = getRandNumber();
-    console.log(roll);
-  }
-});
+//Declare variables to randomize:
+const npcRace = ["Human", "Halfling", "Dwarf", "Elf", "Orc", "Gnome"];
+const npcPowerLevel = ["Weak", "Average", "Strong", "Extreme", "Demigod"];
+const npcGender = ["Male", "Female"];
+const npcTrait = ["Selfish", "Altruistic", "Confident", "Overconfident", "Naive", "Wise", "Friendly", "Quiet", "Shy", "Flirty"];
+const npcBackground = ["was raised in a small village.", "found a magic sword and was granted immense power.", "grew rich from an inheritance.", "has many lovers.", "wants revenge against any and all enemies.", "was raised in an orphange.", "has a curse.", "was sired by a great hero.", "was sired by a great villain", "works in a shop in a small village."];
+//Declare lists of random male names
+const humanMaleNames = ["Frank", "Ryan", "Robert", "Christopher", "Harold", "Arthur", "Gilbert"];
+const halflingMaleNames = ["John", "Paul", "George", "Ringo", "Frodo", "Samwise", "Peregrin", "Meriadoc", "Bilbo"];
+const dwarfMaleNames = ["Gimli", "Gloin", "Thorin", "Dwalin"];
+const elfMaleNames = ["Legolas", "Galadriel", "Elrond", "Glorfindel"];
+const orcMaleNames = ["Garrosh", "Grom", "Orgrim", "Durotan"];
+const gnomeMaleNames = ["Simon", "Alvin", "Theodore", "Garfield"];
+//Declare lists of random female names
+const humanFemaleNames = ["Frank", "Ryan", "Robert", "Christopher", "Harold", "Arthur", "Gilbert"];
+const halflingFemaleNames = ["John", "Paul", "George", "Ringo", "Frodo", "Samwise", "Peregrin", "Meriadoc", "Bilbo"];
+const dwarfFemaleNames = ["Gimli", "Gloin", "Thorin", "Dwalin"];
+const elfFemaleNames = ["Legolas", "Galadriel", "Elrond", "Glorfindel"];
+const orcFemaleNames = ["Garrosh", "Grom", "Orgrim", "Durotan"];
+const gnomeFemaleNames = ["Simon", "Alvin", "Theodore", "Garfield"];
+let charName;
 
-// Display dice rolls
-diceDisplays.addEventListener('')
+//Get the race and gender of the character to determine which name lists to pull from
+const getRace = npcRace[getRandNumber(npcRace.length)];
+const getGender = npcGender[getRandNumber(npcGender.length)];
+const getBiography = npcBackground[getRandNumber(npcBackground.length)];
+
+//Generate a random name from a list of names
+const getName = () => {
+  if (getRace === "Human" && getGender === "Male") {
+    charName = humanMaleNames[getRandNumber(humanMaleNames.length)];
+  } else if (getRace === "Halfling" && getGender === "Male"){
+    charName = halflingMaleNames[getRandNumber(halflingMaleNames.length)];
+  } else if (getRace === "Dwarf" && getGender === "Male"){
+    charName = dwarfMaleNames[getRandNumber(dwarfMaleNames.length)];
+  } else if (getRace === "Elf" && getGender === "Male"){
+    charName = elfMaleNames[getRandNumber(elfMaleNames.length)];
+  } else if (getRace === "Orc" && getGender === "Male"){
+    charName = orcMaleNames[getRandNumber(orcMaleNames.length)];
+  } else if (getRace === "Gnome" && getGender === "Male"){
+    charName = gnomeMaleNames[getRandNumber(gnomeMaleNames.length)];
+  } else if (getRace === "Human" && getGender === "Female") {
+    charName = humanFemaleNames[getRandNumber(humanFemaleNames.length)];
+  } else if (getRace === "Halfling" && getGender === "Female"){
+    charName = halflingFemaleNames[getRandNumber(halflingFemaleNames.length)];
+  } else if (getRace === "Dwarf" && getGender === "Female"){
+    charName = halflingFemaleNames[getRandNumber(halflingFemaleNames.length)];
+  } else if (getRace === "Elf" && getGender === "Female"){
+    charName = elfFemaleNames[getRandNumber(elfFemaleNames.length)];
+  } else if (getRace === "Orc" && getGender === "Female"){
+    charName = orcFemaleNames[getRandNumber(orcFemaleNames.length)];
+  } else if (getRace === "Gnome" && getGender === "Female"){
+    charName = gnomeFemaleNames[getRandNumber(gnomeFemaleNames.length)];
+  }
+  return charName;
+};
+
+// Randomize race/gender/name
+randomizeAllButton.addEventListener('click', function(){
+  getName();
+  heading.innerText = `Character sheet for ${charName}`
+  subheading.innerText = `the ${getGender} ${getRace}`;
+  biography.innerText = `${charName} ${getBiography}`
+});
